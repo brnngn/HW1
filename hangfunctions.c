fce5a9e9d56b306d1d07e70ea9d73aa9d858a7c7
XDuoFan <42943270+XDuoFan@users.noreply.github.com>
2022-09-03 21:45:16 -0400

    Game completed
    left: test, makefile and all the docstrings ect.

--- hangfunctions.c
+++ hangfunctions.c
@@ -5,14 +5,15 @@
 #include <time.h>
 #include <string.h>
 
-// char *options[8] = { "|||========|||\n", 
-//                     "|||     \n", 
-//                     "|||     \n", 
-//                     "|||     \n", 
-//                     "|||     \n", 
-//                     "|||     \n", 
-//                     "|||     \n", 
-//                     "=============="};
+char *options[8] = { "|||========|||\n|||     \n|||     \n|||     \n|||     \n|||     \n|||     \n==============\n",
+"|||========|||\n|||   | \n|||     \n|||     \n|||     \n|||     \n|||     \n==============\n",
+"|||========|||\n|||   | \n|||   O \n|||     \n|||     \n|||     \n|||     \n==============\n",
+"|||========|||\n|||   | \n|||   O \n|||  /  \n|||     \n|||     \n|||     \n==============\n",
+"|||========|||\n|||   | \n|||   O \n|||  /| \n|||     \n|||     \n|||     \n==============\n",
+"|||========|||\n|||   | \n|||   O \n|||  /|\\\n|||     \n|||     \n|||     \n==============\n",
+"|||========|||\n|||   | \n|||   O \n|||  /|\\\n|||  /  \n|||     \n|||     \n==============\n",
+"|||========|||\n|||   | \n|||   O \n|||  /|\\\n|||  / \\\n|||     \n|||     \n==============\n"};
+
 /*
 This function 
 */
@@ -100,8 +101,54 @@ int previous_guess(char guess, bool already_guessed[26]) {
 // /*
 // This function 
 // */
-void print_game_state(const char word[], char game_state[], bool already_guessed[26]) {
-
+void print_game_state(const char word[], char game_state[], bool already_guessed[26],int *missed) {
+    int word_length = strlen(word);
+    printf("%s",options[*missed]);
+    for (size_t i = 0; i < word_length; i++)
+    {
+        printf("%c",game_state[i]);
+    }
+    printf("%s","\nAlready guessed: ");
+    for (size_t i = 0; i < 26; i++)
+    {
+        if (already_guessed[i]){
+            char guessed = 'A';
+            guessed = guessed + i;
+            printf("%c ",guessed);
+        }
+    }
+    printf("%s","\nWhat is your guess? ");
+    char guess = get_guess();
+    if (! previous_guess(guess,already_guessed)){
+        int index = guess - 'A';
+        already_guessed[index] = true;
+    }else{
+        printf("You already guessed this one! Try again:");
+        guess = get_guess();
+    }
+    if (update_game_state(guess, word, game_state)){
+        printf("Good guess!\n");
+    }else{
+        printf("Bad guess!\n");
+        *missed = *missed +1;
+    }
+    printf("Missed: %d", *missed);
+    if ( *missed >= 7){
+        printf("%s",options[7]);
+        printf("You lost and made stick-person sad...\nThe word was ");
+        for (size_t i = 0; i < word_length; i++)
+        {
+            printf("%c",word[i]);
+        }
+        printf(".\n");
+    }
+/*
+_ _ _
+Already guessed:
+What is your guess? e
+Good guess.
+Missed: 0
+*/
 }
 
 
@@ -110,31 +157,39 @@ int main() {
     // printf("User input: ");
     // guess = get_guess();
     // printf("User input is %c", guess);
+    
     const int full_length = 100;
     char word[full_length];
     char game_state[full_length];
     bool already_guessed[26];
     char guess;
+    int missed = 0;
     initialize_game_state(word, game_state, already_guessed);
-    while(!won(word, game_state)){
-        printf("User input: ");
-        guess = get_guess();
-        if (! previous_guess(guess,already_guessed)){
-            int index = guess - 'A';
-            already_guessed[index] = true;
-        }else{
-            printf("You already guessed this one! Try again:");
-            guess = get_guess();
-        }
-        update_game_state(guess, word, game_state);
-        printf("%s",word);
-        printf("%s",game_state);
+    while(!won(word, game_state) && (missed < 7)){
+        print_game_state(word, game_state, already_guessed, &missed);
     }
+    if (won(word, game_state)){
+        printf("You WON and made the stick person happy!:)");
+    }
+
     //printf("User input: ");
     //char guess = get_guess();
     //update_game_state(guess, word, game_state);
     //bool sth[26];
     //char guess;
     //printf("%d", previous_guess(guess, sth));
+    // options[1] = "|||   | \n";
+    // options[2] = "|||   O \n";
+    // options[3] = "|||  /  \n";
+    // options[3] = "|||  /| \n";
+    // options[3] = "|||  /|\\\n";
+    // options[4] = "|||  /  \n";
+    // options[4] = "|||  / \\\n";
+    // for (size_t i = 0; i < 8; i++){
+    //     printf("%s", options[i]);
+    // }
+    // char guessed = 'A';
+    // guessed = guessed + 5;
+    // printf("%c", guessed);
     return 0;
 }
--- hangperson.c
+++ hangperson.c
@@ -10,16 +10,24 @@ CS 372: Operating Systems
 
 void initialize_game_state(char word[], char game_state[], bool already_guessed[26]);
 bool update_game_state(char guess, const char word[], char game_state[]);
-void print_game_state(const char word[], char game_state[], bool already_guessed[26]);
+void print_game_state(const char word[], char game_state[], bool already_guessed[26], int *missed);
 char get_guess(void);
 bool won(const char word[], char game_state[]);
 bool previous_guess(char guess, bool already_guessed[26]);
 
 
 int main(int argc, char **argv) {
-    const int word_length = 100;
-    char word[word_length];
-    char game_state[word_length];
+    const int full_length = 100;
+    char word[full_length];
+    char game_state[full_length];
     bool already_guessed[26];
+    char guess;
+    int missed = 0;
     initialize_game_state(word, game_state, already_guessed);
+    while(!won(word, game_state) && (missed < 7)){
+        print_game_state(word, game_state, already_guessed, &missed);
+    }
+    if (won(word, game_state)){
+        printf("You WON and made the stick person happy!:)");
+    }
 }
